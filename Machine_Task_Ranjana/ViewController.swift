a1766aa74db5a0e7ef7cd073d3d0d2e25f45f3c7
ranjana sharma <ranjana.sharma0109@gmail.com>
2020-04-13 17:00:27 +0530

    Initial Commit

---
+++ Machine_Task_Ranjana/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Machine_Task_Ranjana
+//
+//  Created by Ranjana Sharma on 13/04/20.
+//  Copyright © 2020 Ranjana Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
